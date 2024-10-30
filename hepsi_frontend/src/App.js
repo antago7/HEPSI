6c90396e049a91eed2649dd9edf0ea63ebcb0dff
kutman <kutmanbekbolotov2003@gmail.com>
2024-10-30 21:44:16 +0600

    Homepage added

--- hepsi_frontend/src/App.js
+++ hepsi_frontend/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./components/pages/home/home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  return (
+
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage/>} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
---
+++ hepsi_frontend/src/components/pages/home/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div className="home">
+            <h1>HEPSI</h1>
+        </div>
+    );
+}
+
+export default HomePage;
