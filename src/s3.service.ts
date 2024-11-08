50b5ccefa1b0bdecd86188b5c3cf872f28c7088c
kutman <kutmanbekbolotov2003@gmail.com>
2024-11-08 22:51:48 +0600

    back version controller

--- src/s3.service.ts
+++ src/s3.service.ts
@@ -1,21 +1,13 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
-import { S3Client, GetObjectCommand, ListObjectsV2Command } from '@aws-sdk/client-s3';
-import { GetObjectCommandOutput } from '@aws-sdk/client-s3';
-
+import { Injectable, InternalServerErrorException } from "@nestjs/common";
+import * as AWS from "aws-sdk";
 
 @Injectable()
-export class S3Service {
-    private s3: S3Client;
-
-    constructor() {
-        this.s3 = new S3Client({
-            region: process.env.AWS_REGION,
-            credentials: {
-                accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-            },
-        });
-    }
+export class S3Service { 
+    private s3 = new AWS.S3({
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: process.env.AWS_REGION,
+    });
 
     async getBook(bookKey: string): Promise<Buffer> {
         try {
@@ -23,15 +15,8 @@ export class S3Service {
                 Bucket: process.env.AWS_S3_BUCKET_NAME,
                 Key: bookKey,
             };
-            const command = new GetObjectCommand(params);
-            const data = await this.s3.send(command);
-
-            const chunks: Uint8Array[] = [];
-            const stream = data.Body as ReadableStream;
-            for await (const chunk of stream) {
-                chunks.push(chunk);
-            }
-            return Buffer.concat(chunks);
+            const data = await this.s3.getObject(params).promise();
+            return data.Body as Buffer;
         } catch (error) {
             console.error('Error fetching book:', error);
             throw new InternalServerErrorException('Error fetching book');
@@ -43,10 +28,8 @@ export class S3Service {
             const params = {
                 Bucket: process.env.AWS_S3_BUCKET_NAME,
             };
-            const command = new ListObjectsV2Command(params);
-            const data = await this.s3.send(command);
-
-            return data.Contents?.map(item => item.Key) ?? [];
+            const data = await this.s3.listObjectsV2(params).promise();
+            return data.Contents.map((item) => item.Key);
         } catch (error) {
             console.error('Error listing books:', error);
             throw new InternalServerErrorException('Error listing books');
